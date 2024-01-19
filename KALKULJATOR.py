5319d8e416583be30a654382b0ac2f18bfca657e
Roman <zhukowskiromchik@gmail.com>
2024-01-19 14:01:57 +0300

    V 1.1 Exit add

--- KALKULJATOR.py
+++ KALKULJATOR.py
@@ -4,19 +4,21 @@ while True:
     number_3 = 0.0
 
 
-    action = str(input("Введите действие с числом\n"))
+    action = str(input("Введите действие с числом, либо exit\n"))
 
-    if action == "+":
+    if action == "exit":
+        break
+
+    elif action == "+":
         number_3 = number_1 + number_2
         print(number_1, "+", number_2, "=", number_3) 
         continue
-
+    
     elif action == "-":
         number_3 = number_1 - number_2
         print(number_1, "-", number_2, "=", number_3)
         continue 
         
-
     elif action == "*":
         number_3 = number_1 * number_2
         print(number_1, "*", number_2, "=", number_3)
@@ -26,8 +28,7 @@ while True:
         if number_2 == 0:
             print("На 0 делить нельзя/n")
             continue
-            
-        
+                    
         else:
             number_3 = number_1 / number_2
             print(number_1, "/", number_2, "=", number_3)
