f7e8a09795508dec0a5e91d4fe127659786c2d59
Roman <zhukowskiromchik@gmail.com>
2024-01-19 13:38:18 +0300

    V 1.0 Osnowa progi

---
+++ KALKULJATOR.py
@@ -0,0 +1,43 @@
+while True:
+    number_1 = float(input("Введите первое число\n"))
+    number_2 = float(input("Введите второе число\n"))
+    number_3 = 0.0
+
+
+    action = str(input("Введите действие с числом\n"))
+
+    if action == "+":
+        number_3 = number_1 + number_2
+        print(number_1, "+", number_2, "=", number_3) 
+        continue
+
+    elif action == "-":
+        number_3 = number_1 - number_2
+        print(number_1, "-", number_2, "=", number_3)
+        continue 
+        
+
+    elif action == "*":
+        number_3 = number_1 * number_2
+        print(number_1, "*", number_2, "=", number_3)
+        continue
+
+    elif action == "/":
+        if number_2 == 0:
+            print("На 0 делить нельзя/n")
+            continue
+            
+        
+        else:
+            number_3 = number_1 / number_2
+            print(number_1, "/", number_2, "=", number_3)
+            continue
+
+    elif action == "**":
+        number_3 = number_1 ** number_2
+        print(number_1, "**", number_2, "=", number_3)
+        continue
+
+    else:
+        print("Введенный символ не подходит")
+        continue
