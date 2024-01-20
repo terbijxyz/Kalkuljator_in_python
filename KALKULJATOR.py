757c06af054a2c16ea6f3682f1de187b52d65967
Roman <zhukowskiromchik@gmail.com>
2024-01-20 22:49:49 +0300

    V 1.2 New data entry

--- KALKULJATOR.py
+++ KALKULJATOR.py
@@ -1,10 +1,23 @@
 while True:
-    number_1 = float(input("Введите первое число\n"))
-    number_2 = float(input("Введите второе число\n"))
+    print("___________________________________________________")
+    print("Введите строку формата ((число)(знак)(число)): 2/3")
+    print("Знаки: + , - , * , / , **")
+
+    expression = str(input())
+    razdeliteli = ['/', '+','**','*','-']
+    for razdelitel in razdeliteli:
+        if razdelitel in expression:
+            expression = expression.split(razdelitel)
+            action = razdelitel
+    
+    number_1 = float(expression[0])
+    number_2 = float(expression[1])
     number_3 = 0.0
 
-
-    action = str(input("Введите действие с числом, либо exit\n"))
+    # print(action)
+    # print(expression)
+    # print(number_1)
+    # print(number_2)
 
     if action == "exit":
         break
